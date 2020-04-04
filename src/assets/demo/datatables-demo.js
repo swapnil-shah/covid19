90c971cd589fd7a0dedd15dadffb2ddf3516734d
swapnil-shah <swapnilshah.me@gmail.com>
2020-04-04 15:05:02 -0400

    added datatable usa

--- src/assets/demo/datatables-demo.js
+++ src/assets/demo/datatables-demo.js
@@ -1,19 +1,25 @@
-// Fetch World Data
-// fetch('https://corona.lmao.ninja/countries').then((response) => response.json()).then((countries) => {
-// 	countries.forEach((country) => {
-// 		dataTableWorld.push([ country.country, country.cases, country.deaths, country.recovered, country.active ]);
-// 	});
-// });
+const dateConvert = (date) => {
+	let dateToConvert = new Date(date);
+	year = dateToConvert.getFullYear();
+	month = dateToConvert.getMonth() + 1;
+	dt = dateToConvert.getDate();
+
+	if (dt < 10) {
+		dt = '0' + dt;
+	}
+	if (month < 10) {
+		month = '0' + month;
+	}
+	return month + '/' + dt + '/' + year;
+};
+console.log(dateConvert('2020-04-03T20:00:00Z'));
 
 $(document).ready(function() {
 	$('#dataTableWorld').DataTable({
 		ajax: {
 			url: 'https://corona.lmao.ninja/countries',
 			type: 'GET',
-			dataSrc: function(json) {
-				console.log(json);
-				return json;
-			}
+			dataSrc: ''
 		},
 		columns: [
 			{ data: 'country', title: 'country' },
@@ -24,7 +30,20 @@ $(document).ready(function() {
 		],
 		order: []
 	});
-	// $('#dataTableActivity').DataTable({
-	// 	order: [ [ 0, 'desc' ] ]
-	// });
+	$('#dataTableUsa').DataTable({
+		ajax: {
+			url: 'https://covidtracking.com/api/states',
+			processing: true,
+			type: 'POST',
+			dataSrc: ''
+		},
+		columns: [
+			{ data: 'state', title: 'State' },
+			{ data: 'positive', title: 'Positive', render: $.fn.dataTable.render.number(',') }, //render: $.fn.dataTable.render.number(',') converting to comma separated number
+			{ data: 'death', title: 'Death', render: $.fn.dataTable.render.number(',') },
+			{ data: 'recovered', title: 'Recovered', render: $.fn.dataTable.render.number(',') }, //defaultContent: '0' default value 0 when null
+			{ data: 'lastUpdateEt', title: 'Last Updated' }
+		],
+		order: [ [ 1, 'desc' ] ] //order by column number
+	});
 });
