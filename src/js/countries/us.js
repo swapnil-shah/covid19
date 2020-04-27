6962b03a7f8164be5005ad93e77c9946cb4377f5
swapnil-shah <swapnilshah.me@gmail.com>
2020-04-26 23:21:19 -0400

    dataStored

--- src/js/countries/us.js
+++ src/js/countries/us.js
@@ -1,101 +1,226 @@
 $(document).ready(function() {
-	fetch('https://covid19api.io/api/v1/CasesInAllUSStates').then((response) => response.json()).then(function(states) {
-		dataStored(states);
-	});
-	function dataStored(states) {
-		let chartData = [];
-		let dataSet = states.data[0].table;
-
-		dataSet
-			.filter(function(filterUSA) {
-				return filterUSA.USAState !== 'USA Total';
-			})
-			.forEach(function(state) {
-				chartData.push({
-					label: [ state.USAState ],
-					data: [
-						{
-							x: parseFloat(state.Tot_Cases_1M_Pop.replace(/,/g, '')) * 100000,
-							y: parseFloat(state.Tot_Cases_1M_Pop.replace(/,/g, '')),
-							r: 15
-						}
-					],
-					backgroundColor: 'rgba(255,221,50,0.2)',
-					borderColor: 'rgba(255,221,50,1)'
-				});
-			});
-		console.log(chartData);
-		var bubbleChart = new Chart(document.getElementById('myCountryChart'), {
-			type: 'bubble',
+	//Building pie chart
+	// https://covidtracking.com/api/v1/states/ny/current.json
+	fetch('https://covidtracking.com/api/v1/us/current.json').then((response) => response.json()).then(function(state) {
+		var ctxTotalCases = document.getElementById('pie-chart-total-cases');
+		var ctxhospitliazed = document.getElementById('pie-chart-hospitliazed');
+		var usaDataTotal = [
+			{
+				data: [ state[0].positive, state[0].negative ],
+				backgroundColor: [ '#3e95cd', '#3cba9f' ]
+			}
+		];
+		var usaDataHospitalize = [
+			{
+				data: [ state[0].hospitalizedCurrently, state[0].inIcuCurrently, state[0].onVentilatorCurrently ],
+				backgroundColor: [ '#FF6385', '#FF9F40', '#FFCD56' ]
+			}
+		];
+		var myPieChartTotalCases = new Chart(ctxTotalCases, {
+			type: 'pie',
 			data: {
-				labels: 'USA',
-				datasets: chartData
+				labels: [ 'Positive', 'Negative' ],
+				datasets: usaDataTotal
 			},
 			options: {
-				title: {
-					display: true,
-					text: 'Cases per million'
+				maintainAspectRatio: false,
+				tooltips: {
+					backgroundColor: 'rgb(255,255,255)',
+					bodyFontColor: '#858796',
+					borderColor: '#dddfeb',
+					borderWidth: 1,
+					xPadding: 15,
+					yPadding: 15,
+					displayColors: false,
+					caretPadding: 10,
+					mode: 'label',
+					callbacks: {
+						label: function(tooltipItem, data) {
+							var dataLabel = data.labels[tooltipItem.index];
+							var value =
+								': ' + data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index].toLocaleString();
+							if (Chart.helpers.isArray(dataLabel)) {
+								dataLabel = dataLabel.slice();
+								dataLabel[0] += value;
+							} else {
+								dataLabel += value;
+							}
+							return dataLabel;
+						}
+					}
 				},
-				scales: {
-					yAxes: [
-						{
-							scaleLabel: {
-								display: true,
-								labelString: 'Cases'
-							}
-						}
-					],
-					xAxes: [
-						{
-							scaleLabel: {
-								display: true,
-								labelString: 'Per million'
-							}
-						}
-					]
+				animation: {
+					duration: 1000
 				}
 			}
 		});
-		charts.push(bubbleChart);
-
-		$('#dataTableCountry').DataTable({
-			data: dataSet,
-			pagingType: 'numbers',
-			pageLength: 50,
-			ordering: false,
-			language: {
-				searchPlaceholder: 'e.g. usa'
+		var myPieChartHospitliazed = new Chart(ctxhospitliazed, {
+			type: 'pie',
+			data: {
+				labels: [ 'Hospitalized Currently', 'In ICU Currently', 'On Ventilator Currently' ],
+				datasets: usaDataHospitalize
 			},
-			columns: [
-				{
-					title: 'Name',
-					data: 'USAState'
-				},
-				{
-					title: 'Cases',
-					data: 'TotalCases',
-					render: function(data, type, row) {
-						if (type === 'type' || type === 'sort') {
-							return data;
+			options: {
+				maintainAspectRatio: false,
+				tooltips: {
+					backgroundColor: 'rgb(255,255,255)',
+					bodyFontColor: '#858796',
+					borderColor: '#dddfeb',
+					borderWidth: 1,
+					xPadding: 15,
+					yPadding: 15,
+					displayColors: false,
+					caretPadding: 10,
+					callbacks: {
+						label: function(tooltipItem, data) {
+							var dataLabel = data.labels[tooltipItem.index];
+							var value =
+								': ' + data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index].toLocaleString();
+							if (Chart.helpers.isArray(dataLabel)) {
+								dataLabel = dataLabel.slice();
+								dataLabel[0] += value;
+							} else {
+								dataLabel += value;
+							}
+							return dataLabel;
 						}
-						return `${data}<p class="text-muted small">${row.NewCases}</p>`;
-					}
-				},
-				{
-					title: 'Active Cases',
-					data: 'ActiveCases'
-				},
-				{
-					title: 'Deaths',
-					data: 'TotalDeaths',
-					render: function(data, type, row) {
-						if (type === 'type' || type === 'sort') {
-							return data;
-						}
-						return `${data}<p class="text-muted small">${row.NewDeaths}</p>`;
+					},
+					animation: {
+						duration: 1000
 					}
 				}
-			]
+			}
 		});
-	}
+		$('#selectRegion').on('change', function() {
+			let value = $(this).val();
+			if (value === 'usa') {
+				console.log(usaDataTotal);
+				myPieChartTotalCases.data.datasets = usaDataTotal;
+				myPieChartTotalCases.update();
+				myPieChartTotalCases.options.animation.duration = 1000;
+				myPieChartHospitliazed.data.datasets = usaDataHospitalize;
+				myPieChartHospitliazed.update();
+				myPieChartHospitliazed.options.animation.duration = 1000;
+			} else {
+				fetch('https://covidtracking.com/api/v1/states/' + value + '/current.json', { cache: 'no-cache' })
+					.then((response) => {
+						if (response.ok) {
+							return response.json();
+						} else {
+							throw new Error('BAD HTTP');
+						}
+					})
+					.then((state) => {
+						myPieChartTotalCases.data.datasets = [
+							{
+								data: [ state.positive, state.negative ],
+								backgroundColor: [ '#3e95cd', '#3cba9f' ]
+							}
+						];
+						myPieChartTotalCases.update();
+						myPieChartTotalCases.options.animation.duration = 1000;
+						if (state.hospitalizedCurrently && state.inIcuCurrently && state.onVentilatorCurrently) {
+							myPieChartHospitliazed.data.datasets = [
+								{
+									data: [
+										state.hospitalizedCurrently,
+										state.inIcuCurrently,
+										state.onVentilatorCurrently
+									],
+									backgroundColor: [ '#FF6385', '#FF9F40', '#FFCD56' ]
+								}
+							];
+						} else {
+							myPieChartHospitliazed.data.datasets = [];
+						}
+						myPieChartHospitliazed.update();
+						myPieChartHospitliazed.options.animation.duration = 1000;
+					})
+					.catch((err) => {
+						console.log('ERROR:', err.message);
+					});
+			}
+		});
+
+		// Set new default font family and font color to mimic Bootstrap's default styling
+		(Chart.defaults.global.defaultFontFamily = 'Nunito'),
+			'-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+		Chart.defaults.global.defaultFontColor = '#858796';
+		Chart.plugins.register({
+			afterDraw: function(chart) {
+				if (myPieChartHospitliazed.data.datasets.length === 0) {
+					// No data is present
+					var ctx = myPieChartHospitliazed.chart.ctx;
+					var width = myPieChartHospitliazed.chart.width;
+					var height = myPieChartHospitliazed.chart.height;
+					myPieChartHospitliazed.clear();
+					ctx.save();
+					ctx.textAlign = 'center';
+					ctx.textBaseline = 'middle';
+					ctx.fillText('No data available.', width / 2, height / 2);
+					ctx.restore();
+				}
+			}
+		});
+	});
+	//Building Datatable
+
+	fetch('https://covidtracking.com/api/v1/states/current.json')
+		.then((response) => response.json())
+		.then(function(states) {
+			states
+				.filter(function(posNeg) {
+					return posNeg.positive && posNeg.negative;
+				})
+				.forEach(function(state) {
+					let regionSelect = document.getElementById('selectRegion');
+					regionSelect.options[regionSelect.options.length] = new Option(
+						acronymToFullName(state.state),
+						state.state
+					);
+				});
+			$('#dataTableCountry').DataTable({
+				data: states,
+				pagingType: 'numbers',
+				pageLength: 50,
+				language: {
+					searchPlaceholder: 'e.g. usa'
+				},
+				columns: [
+					{
+						title: 'State',
+						data: 'state',
+						render: function(data, type, row) {
+							if (type === 'type' || type === 'sort') {
+								return data;
+							}
+							return `${acronymToFullName(
+								data
+							)}<p class="text-muted small"> Last updated ${timeDifference(row.dateChecked)}</p>`;
+						}
+					},
+					{
+						title: 'Positive',
+						data: 'positive',
+						render: function(data, type, row) {
+							if (type === 'type' || type === 'sort') {
+								return data;
+							}
+							return `${data.toLocaleString()}<p class="text-muted small">Total tested: <span class="text-body">${row.totalTestResults.toLocaleString()}</span></p><p class="text-muted small">Negative: <span class="text-body">${row.negative.toLocaleString()}</span></p>`;
+						}
+					},
+					{
+						title: 'Recovered',
+						data: 'recovered',
+						render: $.fn.dataTable.render.number(',')
+					},
+					{
+						title: 'Death',
+						data: 'death',
+						render: $.fn.dataTable.render.number(',')
+					}
+				],
+				order: [ [ 1, 'desc' ] ]
+			});
+		});
 });
--- src/js/utilities.js
+++ src/js/utilities.js
@@ -83,3 +83,68 @@ function populationFormat(labelValue) {
 				? (Math.abs(Number(labelValue)) / 1.0e3).toFixed(2) + 'K'
 				: Math.abs(Number(labelValue));
 }
+function acronymToFullName(acronym) {
+	state = {
+		AZ: 'Arizona',
+		AL: 'Alabama',
+		AK: 'Alaska',
+		AR: 'Arkansas',
+		CA: 'California',
+		CO: 'Colorado',
+		CT: 'Connecticut',
+		DC: 'District of Columbia',
+		DE: 'Delaware',
+		FL: 'Florida',
+		GA: 'Georgia',
+		HI: 'Hawaii',
+		ID: 'Idaho',
+		IL: 'Illinois',
+		IN: 'Indiana',
+		IA: 'Iowa',
+		KS: 'Kansas',
+		KY: 'Kentucky',
+		LA: 'Louisiana',
+		ME: 'Maine',
+		MD: 'Maryland',
+		MA: 'Massachusetts',
+		MI: 'Michigan',
+		MN: 'Minnesota',
+		MS: 'Mississippi',
+		MO: 'Missouri',
+		MT: 'Montana',
+		NE: 'Nebraska',
+		NV: 'Nevada',
+		NH: 'New Hampshire',
+		NJ: 'New Jersey',
+		NM: 'New Mexico',
+		NY: 'New York',
+		NC: 'North Carolina',
+		ND: 'North Dakota',
+		OH: 'Ohio',
+		OK: 'Oklahoma',
+		OR: 'Oregon',
+		PA: 'Pennsylvania',
+		RI: 'Rhode Island',
+		SC: 'South Carolina',
+		SD: 'South Dakota',
+		TN: 'Tennessee',
+		TX: 'Texas',
+		UT: 'Utah',
+		VT: 'Vermont',
+		VA: 'Virginia',
+		WA: 'Washington',
+		WV: 'West Virginia',
+		WI: 'Wisconsin',
+		WY: 'Wyoming',
+		AS: 'American Samoa',
+		GU: 'Guam',
+		MP: 'Northern Mariana Islands',
+		PR: 'Puerto Rico',
+		VI: 'U.S. Virgin Islands',
+		UM: 'U.S. Minor Outlying Islands'
+	};
+	if (state[acronym] != null) {
+		return state[acronym];
+	}
+	return acronym;
+}
