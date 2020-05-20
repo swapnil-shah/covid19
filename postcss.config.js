4fbf59bb8223d849f2a9f2d22934bb41d53b65c6
swapnil-shah <swapnilshah.me@gmail.com>
2020-05-20 15:43:33 -0400

    removed purge css and inhtml +2 test

--- postcss.config.js
+++ postcss.config.js
@@ -3,9 +3,6 @@ module.exports = {
 	       require('cssnano')({
             preset: 'default',
         }),
-		require('autoprefixer'),
-		require('@fullhuman/postcss-purgecss')({
-			content: ['./**/*.html']
-		})
+		require('autoprefixer')
 	]
 };
