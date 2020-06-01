94926be20b562ba3d5c32235c7b63bd7de8dc4a5
swapnil-shah <swapnilshah.me@gmail.com>
2020-06-01 14:56:55 -0400

    fixed cors hroku error

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3002;
+app.all('*', function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+	next();
+});
 var distDir = __dirname + '/dist/';
 app.use(express.static(distDir, { extensions: [ 'html' ] }));
 app.listen(port);
