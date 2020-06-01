76e9e3568ec02008c608ef7195f633328dc4b324
swapnil-shah <swapnilshah.me@gmail.com>
2020-06-01 15:19:30 -0400

    removed news from main

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3002;
-app.all('*', function(req, res, next) {
+app.use(function(req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 	next();
 });
+var cors = require('cors');
+app.use(cors());
 var distDir = __dirname + '/dist/';
 app.use(express.static(distDir, { extensions: [ 'html' ] }));
 app.listen(port);
