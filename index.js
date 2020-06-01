0f23e08a7ea8035027218817a388f11cfa5c9013
swapnil-shah <swapnilshah.me@gmail.com>
2020-06-01 14:23:38 -0400

    added cors npm

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3002;
+var cors = require('cors');
+app.use(cors());
 app.listen(port);
 var distDir = __dirname + '/dist/';
 app.use(express.static(distDir, { extensions: [ 'html' ] }));
