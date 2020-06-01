097925969384dbf0d6624709b3d10e3c3533e9ef
swapnil-shah <swapnilshah.me@gmail.com>
2020-06-01 14:54:04 -0400

    cors +3

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3002;
-var cors = require('cors');
+app.all('*', function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+	next();
+});
 var distDir = __dirname + '/dist/';
 app.use(express.static(distDir, { extensions: [ 'html' ] }));
 app.use(cors());
 app.listen(port);
-app.use(function(req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-	next();
-});
