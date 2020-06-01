50598b2eded4718fe8b08fa5269832b95ff29401
swapnil-shah <swapnilshah.me@gmail.com>
2020-05-31 21:37:14 -0400

    back to top script

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -78,6 +78,10 @@
 			'slow'
 		);
 	});
+	$('#return-to-top').click(function() {
+		$('html, body').animate({ scrollTop: 0 }, 600);
+		return false;
+	});
 })(jQuery);
 function lazyLoad() {
 	var lazyloadImages;
